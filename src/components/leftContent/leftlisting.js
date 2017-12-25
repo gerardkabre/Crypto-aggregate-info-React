54310a3762b970328c44308a08a319c1ab1d30d7
Gerard Cabrerizo <gerard.kabre@gmail.com>
2017-12-25 05:53:16 +0100

    added sorting functionality to leftMenu

--- src/components/leftContent/leftlisting.js
+++ src/components/leftContent/leftlisting.js
@@ -1,26 +1,58 @@
 import React from "react";
 import Leftitem from "./leftitem";
 
-let Leftlisting = props => {
-  if (!props.coins) {
-    return <div> Loading... </div>;
+class Leftlisting extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      order: ""
+    };
   }
-  if (props.searchTerm !== "") {
+
+  componentWillUpdate(nextProps, nextState) {
+    switch (nextState.order) {
+      case "PRICE-DECREASE":
+        this.props.coins.sort(
+          (a, b) => parseFloat(b.price_usd) - parseFloat(a.price_usd)
+        );
+        break;
+      case "PRICE-INCREASE":
+        this.props.coins.sort(
+          (a, b) => parseFloat(a.price_usd) - parseFloat(b.price_usd)
+        );
+        break;
+      case "LETTER-DECREASE":
+        this.props.coins.sort((a, b) => b.id.localeCompare(a.id));
+        break;
+      case "LETTER-INCREASE":
+        this.props.coins.sort((a, b) => a.id.localeCompare(b.id));
+        break;
+    }
+  }
+
+  render() {
+    if (!this.props.coins) {
+      return <div> Loading... </div>;
+    }
     return (
       <div className="leftlisting">
         <h2>Choose a currency</h2>
+        <button onClick={() => this.setState({ order: "PRICE-DECREASE" })} />
+        <button onClick={() => this.setState({ order: "PRICE-INCREASE" })} />
+        <button onClick={() => this.setState({ order: "LETTER-INCREASE" })} />
+        <button onClick={() => this.setState({ order: "LETTER-DECREASE" })} />
+
         <div className="list">
           <ul>
-            {props.coins
-              .sort((a, b) => parseFloat(b.price_usd) - parseFloat(a.price_usd))
-              .filter(x => x.id.indexOf(props.searchTerm) >= 0)
+            {this.props.coins
+              .filter(x => x.id.indexOf(this.props.searchTerm) >= 0)
               .map(x => (
                 <Leftitem
                   id={x.id}
                   key={x.id}
                   price={x.price_usd}
-                  onCoinSelect={props.onCoinSelect}
-                  className={props.selectedCoin === x.id ? "selected" : ""}
+                  onCoinSelect={this.props.onCoinSelect}
+                  className={this.props.selectedCoin === x.id ? "selected" : ""}
                 />
               ))}
           </ul>
@@ -28,27 +60,6 @@ let Leftlisting = props => {
       </div>
     );
   }
-
-  return (
-    <div className="leftlisting">
-      <h2>Choose a currency</h2>
-      <div className="list">
-        <ul>
-          {props.coins
-            .sort((a, b) => parseFloat(b.price_usd) - parseFloat(a.price_usd))
-            .map(x => (
-              <Leftitem
-                id={x.id}
-                key={x.id}
-                price={x.price_usd}
-                onCoinSelect={props.onCoinSelect}
-                className={props.selectedCoin === x.id ? "selected" : ""}
-              />
-            ))}
-        </ul>
-      </div>
-    </div>
-  );
-};
+}
 
 export default Leftlisting;
