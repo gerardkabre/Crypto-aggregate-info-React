39b41e71ab61b2a3c474d287191642748c2e8682
Gerard Cabrerizo <gerard.kabre@gmail.com>
2017-12-25 19:41:28 +0100

    reddit posts table implementation and styling

--- src/components/mainContent/Reddit/reddit.js
+++ src/components/mainContent/Reddit/reddit.js
@@ -19,20 +19,37 @@ class Reddit extends React.Component {
   render() {
     return (
       <div className="reddit-container">
-        <h1><span className="blue">{this.props.selectedCoin}</span> Reddit Posts</h1>
-        <div className="reddit-container-inside">
-          {this.state.redditPosts.map(x => (
-            <RedditPost
-              subreddit={x.data.subreddit_name_prefixed}
-              description={x.data.selftext}
-              title={x.data.title}
-              url={x.data.url}
-              comments={x.data.num_comments}
-              upvotes={x.data.ups}
-            />
-          ))}
+        <h1>
+          <span className="blue">{this.props.selectedCoin}</span> Reddit Posts
+        </h1>
+       
+          <div className="reddit-container-inside">
+          
+            <table >
+              <thead>
+                <tr>
+                  <th className="th">Title</th>
+                  <th className="th">Subreddit</th>
+                  <th className="th">Upvotes</th>
+                  <th className="th">Comments</th>
+                </tr>
+              </thead>
+              <tbody className="tbody">
+              {this.state.redditPosts.map(x => (
+                <RedditPost
+                  subreddit={x.data.subreddit_name_prefixed}
+                  description={x.data.selftext}
+                  title={x.data.title}
+                  url={x.data.url}
+                  comments={x.data.num_comments}
+                  upvotes={x.data.ups}
+                />
+              ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-      </div>
+
     );
   }
 }
--- src/components/mainContent/Reddit/redditpost.js
+++ src/components/mainContent/Reddit/redditpost.js
@@ -1,17 +1,14 @@
 import React from "react";
 
 let RedditPost = props => {
-    return (
-        <div className="post">
-          <a href={props.url}>
-          <h3>{props.title}</h3>
-          </a>
-          <div className="post-descriptions ">
-             <h4>{props.subreddit}</h4> <p> upvotes : {props.upvotes} | comments: {props.comments}</p>
-
-          </div>
-        </div>
-      )
-    }
+  return (
+    <tr>
+      <td class="blue">{props.title}</td>
+      <td>{props.subreddit}</td>
+      <td>{props.upvotes}</td>
+      <td>{props.comments}</td>
+    </tr>
+  );
+};
 
 export default RedditPost;
